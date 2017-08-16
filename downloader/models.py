62262983338a5fd2336a3019e1782e7478b4e0b0
metr1ckzu <metr1ckzu@gmail.com>
2017-08-16 15:31:17 +0600

    added model for video

--- downloader/models.py
+++ downloader/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Video(models.Model):
+    youtube_id = models.CharField(max_length=100)
+    url = models.URLField(max_length=100)
+    audio_filename = models.CharField(max_length=255, null=True, blank=True)
