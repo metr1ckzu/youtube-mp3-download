33d0d14a5e7e21a0336781f8d2c01b63577a9786
metr1ckzu <metr1ckzu@gmail.com>
2017-08-16 20:48:59 +0600

    changed video class, added user class in models

--- downloader/models.py
+++ downloader/models.py
@@ -5,7 +5,22 @@ from django.db import models
 
 # Create your models here.
 
+
 class Video(models.Model):
+
     youtube_id = models.CharField(max_length=100)
     url = models.URLField(max_length=100)
     audio_filename = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.youtube_id
+
+
+class User(models.Model):
+
+    video = models.ForeignKey(Video)
+    user_email = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user_email
+
