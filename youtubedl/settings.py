ff20446afc5605f7baf54fe004a76151d8ad25fa
metr1ckzu <metr1ckzu@gmail.com>
2017-08-17 16:23:29 +0600

    added smtp credentials

--- youtubedl/settings.py
+++ youtubedl/settings.py
@@ -131,7 +131,7 @@ MEDIA_URL = '/media/'
 # SMTP settings (DRAFT)
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'XXXX'
-EMAIL_HOST_PASSWORD = 'XXXX'
-EMAIL_PORT = 587
+EMAIL_HOST = 'mail.smtp2go.com'
+EMAIL_HOST_USER = 'youtubedl@youtubedl.com'
+EMAIL_HOST_PASSWORD = 'fwv0nyjReB7M'
+EMAIL_PORT = 2525
