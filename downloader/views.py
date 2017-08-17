0050f2c4b945454b976931894007353aa43b0bae
metr1ckzu <metr1ckzu@gmail.com>
2017-08-17 15:27:03 +0600

    started making views

--- downloader/views.py
+++ downloader/views.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Video, User
 
 # Create your views here.
+
+def submit(request):
+    template_name = 'downloader/index.html'
+
+    return render(request, template_name)
