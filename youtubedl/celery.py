f732e2e7976609d6190013eec8c3b7689284f43f
metr1ckzu <metr1ckzu@gmail.com>
2017-08-18 14:16:20 +0600

    removed namespace for celery

--- youtubedl/celery.py
+++ youtubedl/celery.py
@@ -11,7 +11,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youtubedl.settings')
 
 app = Celery('youtubedl')
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings')
 app.autodiscover_tasks()
 #app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
